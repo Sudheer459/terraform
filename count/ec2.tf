60099536c6b197bff9f57479e792b44ee2ab2f7c
Sudheer459 <sudheersudhi.99@gmail.com>
2024-05-08 19:59:47 +0530

    message

---
+++ count/ec2.tf
@@ -0,0 +1,34 @@
+# resource <resource-type> <resource-name>
+resource "aws_instance" "db" {
+    count = 3
+    ami = "ami-07caf09b362be10b8"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh3.id]
+    tags = {
+        Name = "db"
+    }
+}
+
+ resource "aws_security_group" "allow_ssh3" {
+    name = "allow_ssh3"
+    description = "allowing SSH access"
+
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    egress {
+        from_port        = 0 # from 0 to 0 means, opening all protocols
+        to_port          = 0
+        protocol         = "-1" # -1 all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name = "allow_ssh3"
+        createdby = "sudheer"
+    }
+}
